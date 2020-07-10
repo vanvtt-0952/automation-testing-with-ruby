f758eba98193aa5900a5ca20a6c28c9b722058a4
vanvtt-0952 <vu.thi.tran.van@sun-asterisk.com>
2020-07-10 14:34:12 +0700

    delete example 1

--- example1/driver/driver_variable.rb
+++
@@ -1,5 +0,0 @@
-class DriverVariable
- root_path = "/home/vu.thi.tran.van/Training/AutomationTest/selenium/Ruby/workspace/example_code_slides/automation-testing-with-ruby/example1/driver"
-  CHROME_DRIVER = root_path + "/chromedriver"
-  FIREFOX_DRIVER = root_path + "/geckodriver"
-end
